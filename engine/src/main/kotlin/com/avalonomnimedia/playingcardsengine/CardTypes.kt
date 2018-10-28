ebcc3e800a051d8c70b0e64852836add68f034a3
Joe Shea <jsshea82@gmail.com>
2018-10-28 10:46:15 +0100

    Adding basic card attributes

---
+++ engine/src/main/kotlin/com/avalonomnimedia/playingcardsengine/CardTypes.kt
@@ -0,0 +1,24 @@
+package com.avalonomnimedia.playingcardsengine
+
+enum class Suit {
+    DIAMONDS,
+    SPADES,
+    CLUBS,
+    HEARTS
+}
+
+enum class Value {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING
+}
