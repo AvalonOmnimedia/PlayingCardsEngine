a9ab1b8e965d7501be14d11187273370b780c987
Joe Shea <jsshea82@gmail.com>
2018-10-28 17:01:02 +0100

    Enable coroutines in the CLI module

--- war-cli/build.gradle.kts
+++ war-cli/build.gradle.kts
@@ -1,9 +1,18 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+
 plugins {
     kotlin("jvm")
 }
 
 dependencies {
     implementation(project(":engine"))
+
     implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.30.2")
+
     testImplementation("junit:junit:4.12")
+}
+
+kotlin {
+    experimental.coroutines = Coroutines.ENABLE
 }
