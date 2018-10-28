f368465006d01347098ff8331e3e991f4b62fa32
Joe Shea <jsshea82@gmail.com>
2018-10-28 11:19:26 +0100

    Adding Dokka to automatically create documentation

--- engine/build.gradle.kts
+++ engine/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `maven-publish`
     kotlin("jvm")
+    id("org.jetbrains.dokka") version "0.9.16"
 }
 
 dependencies {
@@ -8,10 +9,23 @@ dependencies {
     testImplementation("junit:junit:4.12")
 }
 
+val dokka by tasks.getting(org.jetbrains.dokka.gradle.DokkaTask::class) {
+    outputFormat = "html"
+    outputDirectory = "$buildDir/javadoc"
+}
+
+val dokkaJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    description = "Assembles Kotlin docs with Dokka"
+    classifier = "javadoc"
+    from(dokka)
+}
+
 publishing {
     publications {
         create("default", MavenPublication::class.java) {
             from(components["java"])
+            artifact(dokkaJar)
         }
     }
     repositories {
