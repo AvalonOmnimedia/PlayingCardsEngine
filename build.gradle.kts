2a17ca62a6bf051659f7d238e3ac6663065e7d7b
Joe Shea <jsshea82@gmail.com>
2022-02-05 23:07:47 +0100

    Remove deprecated build stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     base
-    kotlin("jvm") version "1.3.11" apply false
     id("org.jetbrains.kotlin.jvm") version "1.5.20" apply false
 }
 
 buildscript {
     repositories {
-        jcenter()
+        mavenCentral()
     }
     dependencies {
         classpath("com.github.dcendents:android-maven-gradle-plugin:1.4.1")
@@ -18,15 +17,6 @@ allprojects {
     version = "0.2"
 
     repositories {
-        jcenter()
-    }
-
-    apply(plugin="com.github.dcendents.android-maven")
-}
-
-dependencies {
-    // Make the root project archives configuration depend on every subproject
-    subprojects.forEach {
-        archives(it)
+        mavenCentral()
     }
 }
