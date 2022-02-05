d6b4423f68ade2e862ee28d7f4ff860c88a79a18
Joe Shea <jsshea82@gmail.com>
2022-02-06 00:15:28 +0100

    Create new module for May I

---
+++ may-i/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    application
+    kotlin("jvm")
+}
+
+dependencies {
+    implementation(project(":engine"))
+
+    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+
+    implementation("com.github.ajalt.clikt:clikt:3.4.0")
+
+    testImplementation("io.mockk:mockk:1.12.2")
+    testImplementation("junit:junit:4.13.2")
+}
