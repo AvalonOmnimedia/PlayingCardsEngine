95716a3591c5c80678cf0e8e461bcfe975c5fbb8
Joe Shea <jsshea82@gmail.com>
2022-02-11 08:35:54 +0100

    Improve card printing

--- engine/src/main/kotlin/com/avalonomnimedia/playingcardsengine/Card.kt
+++ engine/src/main/kotlin/com/avalonomnimedia/playingcardsengine/Card.kt
@@ -2,29 +2,37 @@ package com.avalonomnimedia.playingcardsengine
 
 data class Card(val suit: Suit, val value: Value) {
     override fun toString(): String {
-        return "$value of $suit"
+        return "$value$suit"
     }
 }
 
-enum class Suit {
-    DIAMONDS,
-    SPADES,
-    CLUBS,
-    HEARTS
+enum class Suit(val symbol: String) {
+    DIAMONDS("♦️"),
+    SPADES("♠️"),
+    CLUBS("♣️"),
+    HEARTS("♥️");
+
+    override fun toString(): String {
+        return symbol
+    }
 }
 
-enum class Value {
-    ACE,
-    TWO,
-    THREE,
-    FOUR,
-    FIVE,
-    SIX,
-    SEVEN,
-    EIGHT,
-    NINE,
-    TEN,
-    JACK,
-    QUEEN,
-    KING
+enum class Value(val value: String) {
+    ACE("A"),
+    TWO("2"),
+    THREE("3"),
+    FOUR("4"),
+    FIVE("5"),
+    SIX("6"),
+    SEVEN("7"),
+    EIGHT("8"),
+    NINE("9"),
+    TEN("10"),
+    JACK("J"),
+    QUEEN("Q"),
+    KING("K");
+
+    override fun toString(): String {
+        return value
+    }
 }
